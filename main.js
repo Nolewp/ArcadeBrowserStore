c2b175ab781a824ae24d3c36a3e4c9c8d178aa04
Jack Ochs <jochs4fun@gmail.com>
2025-01-12 09:37:38 -0500

    Saving information on the number of saves in order to give users different alerts based on previous use

--- main.js
+++ main.js
@@ -3,12 +3,13 @@ function parseURLParams() {
     const params = new URLSearchParams(window.location.search); // Extract query parameters
     const title = params.get('title') ? decodeURIComponent(params.get('title')) : 'Title edit me!';
     const code = params.get('code') ? decodeURIComponent(params.get('code')) : '// Write your Arcade Script here';
-    return { title, code };
+    const saves = params.get('saves') ? decodeURIComponent(params.get('saves')) : 0;
+    return { title, code, saves };
 }
 
 
 // Extract and use URL parameters
-const { title, code } = parseURLParams();
+var { title, code, saves } = parseURLParams();
 document.getElementById('pageTitleInput').value = title; // Set the title input value
 document.title = title; // Update the browser's title
 
@@ -34,14 +35,19 @@ function copyToClipboard(text) {
       });
   }
 
- // Update button text
 document.getElementById('runCode').addEventListener('click', () => {
     const updatedCode = encodeURIComponent(editor.getValue());
     const updatedTitle = encodeURIComponent(document.getElementById('pageTitleInput').value);
-    const newURL = `${window.location.origin}${window.location.pathname}?title=${updatedTitle}&code=${updatedCode}`;
+    const updatedSavesNumber =  encodeURIComponent(saves +=1)
+    const newURL = `${window.location.origin}${window.location.pathname}?title=${updatedTitle}&saves=${updatedSavesNumber}&code=${updatedCode}`;
     history.replaceState(null, '', newURL); // Update the URL without refreshing
     document.title = document.getElementById('pageTitleInput').value; // Update browser title dynamically
-    alert('Title and Code saved to URL!');
+    if (saves == 1){
+      alert('Welcome to the Arcade Browser Store! After saving, the URL is encoded with the information in your browser. This makes it easy to share the URL or bookmark it for quick access later.');
+    }
+    else{
+      alert('Title and Code saved to URL!');
+    }
     copyToClipboard(newURL);
 
 
